dd77023f06c7dc8970dea20142edee9e8baeb399
Jonathan Zavala <jontzavala@gmail.com>
2021-05-18 16:54:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let mn = document.querySelector("main#main");
+mn.remove()
+
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = 'Jonathan Zavala is the champion'
